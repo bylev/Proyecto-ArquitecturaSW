b4a22fe4c726ca87cdc898163225f039eda04b09
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-06-20 00:57:22 -0600

    feat: Implementación de la API

---
+++ TransGGP.API/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using TransGGP.Application.Interfaces;
+using TransGGP.Application.Services;
+using TransGGP.Infrastructure.Data;
+using TransGGP.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios
+builder.Services.AddControllers();
+
+// DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+// Inyección de dependencias
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<ClienteService>();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "docs/swagger/{documentName}.json";
+    });
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/docs/swagger/v1.json", "TransGGP API");
+        options.RoutePrefix = "docs";
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
