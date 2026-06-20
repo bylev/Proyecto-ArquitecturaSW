776d1af1cdea5cb17b84a05e077ebb1d9e590ca2
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-06-20 00:57:22 -0600

    feat: Implementación de la API

---
+++ TransGGP.API/Controllers/ClientesAPI.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TransGGP.Application.DTOs;
+using TransGGP.Application.Services;
+using TransGGP.Domain.Models;
+
+namespace TransGGP.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesApiController : ControllerBase
+    {
+        private readonly ClienteService _clienteService;
+
+        public ClientesApiController(ClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        
+        [HttpGet]
+        public ActionResult<List<Cliente>> ObtenerTodos()
+        {
+            var clientes = _clienteService.ObtenerTodos();
+            return Ok(clientes);
+        }
+
+      
+        [HttpPost]
+ 
+        public ActionResult<Cliente> CrearCliente([FromBody] ClienteCreateDto clienteDto)
+        {
+            if (string.IsNullOrWhiteSpace(clienteDto.Nombre))
+                return BadRequest("El nombre del cliente es requerido");
+
+            var cliente = new Cliente
+            {
+                Nombre = clienteDto.Nombre,
+                FechaCreacion = DateTime.Now
+            };
+
+            var clienteCreado = _clienteService.RegistrarCliente(cliente);
+            return CreatedAtAction(nameof(ObtenerTodos), new { id = clienteCreado.Id }, clienteCreado);
+        }
+    }
+}
