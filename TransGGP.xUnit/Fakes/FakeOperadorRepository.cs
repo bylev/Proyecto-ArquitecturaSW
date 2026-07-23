a2226fbf983390ca787f818a5ea547973abe0a20
bylev <michelle.camara@tecdesoftware.edu.mx>
2026-07-22 18:19:50 -0600

    test: Añadir prueba unitaria para Operadores

---
+++ TransGGP.xUnit/Fakes/FakeOperadorRepository.cs
@@ -0,0 +1,34 @@
+﻿using TransGGP.Application.Interfaces;
+using TransGGP.Domain.Models;
+
+namespace TransGGP.xUnit.Fakes;
+
+public class FakeOperadorRepository : IOperadorRepository
+{
+    public List<Operador> Operadores { get; } = new();
+
+    public List<Operador> ObtenerTodos()
+    {
+        return Operadores;
+    }
+
+    public Operador? ObtenerPorId(int id)
+    {
+        return Operadores.FirstOrDefault(operador => operador.Id == id);
+    }
+
+    public void Agregar(Operador operador)
+    {
+        Operadores.Add(operador);
+    }
+
+    public void Eliminar(int id)
+    {
+        var operador = ObtenerPorId(id);
+
+        if (operador is not null)
+        {
+            Operadores.Remove(operador);
+        }
+    }
+}
